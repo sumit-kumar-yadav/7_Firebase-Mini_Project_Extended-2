967bcfcca32cd606b7c45f8db34bd1a009492ff3
Sumit <sumityadav0376@gmail.com>
2021-04-23 13:43:59 +0530

    Increase quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,7 +9,17 @@ class CartItem extends React.Component {
       qty: 1,
       img: ''
     }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
+
+//   increaseQuantity = function(){
+//     console.log('this', this.state);
+//   }
+
+  increaseQuantity = () => {
+    console.log('this', this.state);
+  }
+
   render () {
     const { price, title, qty } = this.state;
     return (
@@ -23,9 +33,30 @@ class CartItem extends React.Component {
           <div style={ { color: '#777' } }>Qty: {qty} </div>
           <div className="cart-item-actions">
             {/* Buttons */}
-            <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg" />
-            <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1665/1665612.svg" />
-            <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" />
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+            //   1) Either bind this here and use normal function in class
+            //   onClick={this.increaseQuantity.bind(this)}
+
+            // 2) Or bind in constructor itself and create property and use normal function in class
+            // onClick={this.increaseQuantity}
+
+            // 3) Or use arrow function to bind this automatically
+            onClick={this.increaseQuantity}
+
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+            />
           </div>
         </div>
       </div>
