33700a9301296874337c54845f1cee862eb7d004
Sumit <sumityadav0376@gmail.com>
2021-04-25 14:07:54 +0530

    Creating the Cart component

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,28 +10,28 @@ class CartItem extends React.Component {
       img: ''
     }
     // this.increaseQuantity = this.increaseQuantity.bind(this);
-    this.testing();
+    // this.testing();
   }
 
 
-  testing () {
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('done');
-      }, 5000);
-    })
+  // testing () {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve('done');
+  //     }, 5000);
+  //   })
 
-    promise.then(() => {
-      // setState acts like a synchronus call
-      this.setState({ qty: this.state.qty + 10 });
+  //   promise.then(() => {
+  //     // setState acts like a synchronus call
+  //     this.setState({ qty: this.state.qty + 10 });
 
-      this.setState({ qty: this.state.qty + 10 });
+  //     this.setState({ qty: this.state.qty + 10 });
 
-      this.setState({ qty: this.state.qty + 10 });
+  //     this.setState({ qty: this.state.qty + 10 });
 
-      console.log('state', this.state);
-    });
-  }
+  //     console.log('state', this.state);
+  //   });
+  // }
 
 
   increaseQuantity = () => {
