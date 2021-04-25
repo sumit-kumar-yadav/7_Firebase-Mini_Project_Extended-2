38254392d757039bf2a276feac1f9fb2b2002520
Sumit <sumityadav0376@gmail.com>
2021-04-25 16:28:41 +0530

    Props

--- src/Cart.js
+++ src/Cart.js
@@ -2,14 +2,52 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 99,
+          title: 'Watch',
+          qty: 1,
+          img: '',
+          id: 1
+        },
+        {
+          price: 999,
+          title: 'Mobile Phone',
+          qty: 10,
+          img: '',
+          id: 2
+        },
+        {
+          price: 999,
+          title: 'Laptop',
+          qty: 4,
+          img: '',
+          id: 3
+        }
+      ]
+    }
+  }
   render () {
-      const arr = [1, 2, 3, 4, 5];
+    const { products } = this.state;
     return (
       <div className="cart">
-        { arr }
-        { arr.map((item) => {
-            return item + 5;
-        }) }
+        {products.map((product) => {
+          return (
+        //   <CartItem qty={1} price={99} title={"watch"} img={''} />
+        
+            <CartItem
+              product={product}
+              key={product.id}
+        //   fun={() => console.log('Even pass function, jsx, component, etc')}
+        //   isLoggedIn={false}
+        //   jsx={<h1>Pass jsx</h1>}
+        //   comp={<CartItem />}
+            />
+          )
+        })}
       </div>
     );
   }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,59 +1,21 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: ''
-    }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    // this.testing();
-  }
-
-
-  // testing () {
-  //   const promise = new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve('done');
-  //     }, 5000);
-  //   })
-
-  //   promise.then(() => {
-  //     // setState acts like a synchronus call
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     console.log('state', this.state);
-  //   });
-  // }
-
-
+  
   increaseQuantity = () => {
-    // this.state.qty += 1;   // This will increase the value but not update the data in component
+    // this.state.qty += 1;
     // console.log('this', this.state);
-
-    // If setState() is called, then component is rerendered after updating values
-            // setState form 1  -- Use if previous state data is not required eg. title
+    // setState form 1
     // this.setState({
     //   qty: this.state.qty + 1
-    // }, () => {
-    //   console.log('this.state', this.state);
-    // });
+    // }, () => {});
 
-            // setState form 2 - if prevState required use this
+    // setState form 2 - if prevState required use this
     this.setState((prevState) => {
-        return {
-          qty: prevState.qty + 1
-        }
-      }, () => {
-        console.log('this.state', this.state);
-      });
+      return {
+        qty: prevState.qty + 1
+      }
+    });
   }
 
   decreaseQuantity = () => {
@@ -69,13 +31,12 @@ class CartItem extends React.Component {
       }
     });
   }
-
-
   render () {
-    const { price, title, qty } = this.state;
-    console.log('render');
+    console.log('this.props', this.props);
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
+        {this.props.jsx}
         <div className="left-block">
           <img style={styles.image} />
         </div>
@@ -90,7 +51,6 @@ class CartItem extends React.Component {
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/992/992651.svg"
               onClick={this.increaseQuantity}
-
             />
             <img
               alt="decrease"
