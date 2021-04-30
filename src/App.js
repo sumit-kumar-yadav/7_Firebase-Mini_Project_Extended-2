10098f6591844dbb4e3f556c5032d9900820adfd
Sumit <sumityadav0376@gmail.com>
2021-04-30 14:10:51 +0530

    Rendering data from Firebase I, II

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase';
 
 class App extends React.Component {
 
   constructor () {
     super();
     this.state = {
-      products: [
-        {
-          price: 99,
-          title: 'Watch',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-          id: 1
-        },
-        {
-          price: 999,
-          title: 'Mobile Phone',
-          qty: 10,
-          img: 'https://images.unsplash.com/photo-1520923642038-b4259acecbd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1306&q=80',
-          id: 2
-        },
-        {
-          price: 999,
-          title: 'Laptop',
-          qty: 4,
-          img: 'https://images.unsplash.com/photo-1504707748692-419802cf939d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1330&q=80',
-          id: 3
-        }
-      ]
+      products: [],
+      loading: true
     }
   }
+
+     // Reading data from firebase when component is mounted (Does not update automatically)
+  // componentDidMount() {
+  //   firebase
+  //     .firestore()
+  //     .collection("products")   // Collection name
+  //     .get()   // Returns a promise
+  //     .then(snapshot => {
+  //       const products = snapshot.docs.map(doc => {
+  //         const data = doc.data();
+  //         data["id"] = doc.id;
+  //         return data;
+  //       });
+  //       this.setState({ products: products, loading: false });
+  //     });
+  // }
+
+     // Reading data from firebase when component is mounted (Updates automatically)
+  componentDidMount() {
+    firebase
+      .firestore()
+      .collection("products")
+      .onSnapshot(snapshot => {  // onSnapshot func is called whenever data changes in firebase
+        const products = snapshot.docs.map(doc => {
+          const data = doc.data();
+          data["id"] = doc.id;
+          return data;
+        });
+        this.setState({ products: products, loading: false });
+      });
+  }
+
+
 
   handleIncreaseQuantity = (product) => {
     console.log('Heyy please inc the qty of ', product);
@@ -104,7 +117,7 @@ class App extends React.Component {
 
 
   render () {
-    const { products } = this.state;
+    const { products, loading } = this.state;
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
@@ -114,6 +127,7 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+        {loading && <h1>Loading Products...</h1>}
         <div style={ {padding: 10, fontSize: 20} }>TOTAL: {this.getCartTotal()} </div>
       </div>
     );
